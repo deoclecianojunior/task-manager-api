8c41d6267bd86a9e83d3cb89e11b791509e58fe1
Deocleciano Júnior <deocleciano@hotmail.com>
2018-09-29 15:35:12 +0000

    Refactoring the authorization and adding user_singed_in? method

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Authenticable do
 	let(:app_controller) { subject }
 
 	describe '#current_user' do
-		let(:user ) {create(:user)}
+		let(:user) {create(:user)}
 
 		before do
 			req = double(:headers => { 'Authorization' => user.auth_token })
@@ -42,6 +42,25 @@ RSpec.describe Authenticable do
 			it 'returns the json data for the errors' do
 				expect(json_body).to have_key(:errors)
 			end
+		end
+	end
+
+	describe '#user_logged_in?' do
+		context 'when there is a user logged in ' do
+			before do
+				user = create(:user)
+				allow(app_controller).to receive(:current_user).and_return(user)
+			end
+			
+			it { expect(app_controller.user_logged_in?).to be true}
+		end
+
+		context 'when there is no user logged in ' do
+			before do
+				allow(app_controller).to receive(:current_user).and_return(nil)
+			end
+
+			it { expect(app_controller.user_logged_in?).to be false}
 		end
 	end
 end
