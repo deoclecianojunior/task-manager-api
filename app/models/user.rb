605fe748501b4f53eec6335efd60f6168d250cdd
Deocleciano Júnior <deocleciano@hotmail.com>
2018-10-01 17:28:13 +0000

    Adding tasks controller and show action with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   validates_uniqueness_of :auth_token
-
   before_create :generate_authentication_token!
+
+  has_many :tasks, dependent: :destroy
 
   def info
   	"#{email} - #{created_at} - Token: #{Devise.friendly_token}"
