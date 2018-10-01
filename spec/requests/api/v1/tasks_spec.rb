cb98a2a814a3f565a2ebe24d23434b56516a1960
Deocleciano Júnior <deocleciano@hotmail.com>
2018-10-01 17:39:06 +0000

    Adding show action to the tasks controller with tests

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Task API', type: :request do
-	before { host! 'api.task-manager.test' }
+	before { host! 'api.taskmanager.test' }
 
 	let!(:user) { create(:user) }
 	let(:headers) do
@@ -20,6 +20,20 @@ RSpec.describe 'Task API', type: :request do
 
 		it 'returns 5 tasks from database' do
 			expect(json_body[:tasks].count).to eq(5)
+		end
+	end
+
+	describe 'GET /tasks/:id' do
+		let(:task) { FactoryGirl.create(:task, user_id: user.id) }
+
+		before { get "/tasks/#{task.id}", params: {}, headers: headers }
+
+		it 'returns status code 200' do
+			expect(response).to have_http_status(200)
+		end
+
+		it 'returns the json for task' do
+			expect(json_body[:title]).to eq(task.title)
 		end
 	end
 end
